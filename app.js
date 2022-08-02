ed5ffe865c9dc9c50eb2b01ce3ae47588610b473
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2022-08-02 16:16:31 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 let txtInput = document.querySelector("#txt-input");
 let submitBtn = document.querySelector("#submit-btn");
-let output = document.querySelector("#ouput");
+let output = document.querySelector("#output");
+let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 submitBtn.addEventListener("click", clickHandler);
 
+function errorHandler(error) {
+  alert("Something wrong with the server please try again later");
+}
+
 function clickHandler() {
-  console.log(txtInput.value);
+  if (!txtInput.value == "") {
+    let text = url + "?text=" + txtInput.value;
+
+    fetch(text)
+      .then((response) => response.json())
+      .then((json) => (output.innerText = json.contents.translated))
+      .catch(errorHandler);
+    txtInput.value = "";
+  }
 }
